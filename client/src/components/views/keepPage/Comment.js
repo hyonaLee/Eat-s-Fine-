8741f01e20e7f03ffe5eb10cc8e29e9a741ba716
donwon <seo77j@gmail.com>
2021-08-17 10:12:34 +0900

    08171012 서동원 코멘트오류 수정

--- client/src/components/views/keepPage/Comment.js
+++ client/src/components/views/keepPage/Comment.js
@@ -1,24 +1,22 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { addComment } from "../../../_actions/user_actions";
-import SingleComment from "./SingleComment";
-function Comment({ index }) {
+function Comment({ id }) {
   const user = useSelector((state) => state.user);
   const [text, setText] = useState("");
   const dispatch = useDispatch();
-  console.log("코멘트인덱스",index)
+
+
   const onchangeText = (e) => {
     setText(e.target.value);
   };
-  // console.log("코멘트의 리스트",list)
   const onSubmit = (e) => {
-    console.log("등록버튼클릭 유저킵정보", user.userData.keep[index].id);
     e.preventDefault(); //페이지 새로고침 안되게함
 
     const data = {
-      storeid: user.userData.keep[index].id,
+      storeid:id,
       content: text,
       writer: user.userData._id,
     };
@@ -26,18 +24,15 @@ function Comment({ index }) {
     dispatch(addComment(data));
     setText("");
   };
-
   return (
     <div>
-      <form style={{ display: "flex" }} onSubmit>
-        <textarea
-          style={{ width: "400px", borderRadius: " 5px" }}
-          onChange={onchangeText}
-          value={text}
-          placeholder="기대평, 후기 등 코멘트 작성공간"
-        />
-        <button onClick={onSubmit}>등록</button>
-      </form>
+      <textarea
+        style={{ width: "400px", borderRadius: " 5px" }}
+        onChange={onchangeText}
+        value={text}
+        placeholder="기대평, 후기 등 코멘트 작성공간"
+      />
+      <button onClick={onSubmit}>등록</button>
     </div>
   );
 }
