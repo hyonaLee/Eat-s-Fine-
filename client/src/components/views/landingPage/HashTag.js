bd02133a2ad15bb5d01041b20376580b82216f54
hyunha Lee <fly9739@gmail.com>
2021-08-17 17:11:27 +0900

    20210817 이현하 해시태그 셔플 기능 추가

--- client/src/components/views/landingPage/HashTag.js
+++ client/src/components/views/landingPage/HashTag.js
@@ -2,13 +2,28 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useApplicationContext } from "../../../contexts/weatherAndMap_context";
 
+
+
 function HashTag() {
-const { sky, setSky, currentMenu, setCurrentMenu, listNum, setListNum } = useApplicationContext();
+const { sky, setSky, currentMenu, setCurrentMenu, listNum, setListNum, changeNum, setChangeNum } = useApplicationContext();
   const CurrentWeatherData = { sky };
+
+  // console.log(currentMenu)
+  // console.log(currentMenu.length)
+  
+function randomNum () {
+  while(changeNum.length < 4) {
+      let num = parseInt(Math.random()*currentMenu.length);
+      if(changeNum.indexOf(num) <0) {
+        changeNum.push(num)
+      }
+    }
+    console.log(changeNum);
+}
+
   return (
     <HashDiv>
       <HashH2>
-      
         <span>#{currentMenu[listNum[0]]} </span>
         <span>#{currentMenu[listNum[1]]} </span>
         <span>#{currentMenu[listNum[2]]} </span>
--- client/src/components/views/landingPage/RecomendBtn.js
+++ client/src/components/views/landingPage/RecomendBtn.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 import styled from 'styled-components';
 import { useApplicationContext } from "../../../contexts/weatherAndMap_context";
 
 function RecomendBtn() {
-const { listNum, setListNum } = useApplicationContext();
+const { listNum, setListNum, currentMenu, setCurrentMenu } = useApplicationContext();
+const [ changeNum, setChangeNum] = useState([]);
+
+function randomNum () {
+  while(changeNum.length < 4) {
+      let num = parseInt(Math.random()*currentMenu.length);
+      if(changeNum.indexOf(num) <0) {
+        changeNum.push(num)
+      }
+    }
+    console.log(changeNum);
+}
+
 
 function clickMore() {
-  setListNum(
-    listNum.map(index => index+4)
-  )
-}
+    randomNum ()
+    setListNum(changeNum)}
+  console.log(listNum)
+
     return (
         <BtnDiv>
             <RecomendBtnStyle onClick={clickMore}>
