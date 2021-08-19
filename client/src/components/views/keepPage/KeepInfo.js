bc1fd31b8f16ff8949caf519123ede9d0dce72e8
hyunha Lee <fly9739@gmail.com>
2021-08-19 15:46:10 +0900

    20210819 이현하 찜목록 수정

--- client/src/components/views/keepPage/KeepInfo.js
+++ client/src/components/views/keepPage/KeepInfo.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { deleteKeep } from "../../../_actions/user_actions";
-import Comment from "./Comment";
+import styled from "styled-components";
+import Comment from "./Comment"
+
 
 function KeepInfo({ list }) {
   const dispatch = useDispatch();
@@ -18,38 +20,72 @@ function KeepInfo({ list }) {
   }
 
   return (
-    <div>
-      <ul>
-        {list.map((item, index) => (
-          <li style={{ border: "1px solid black" }}>
-            <div
-              onClick={() =>
-                window.open(`http://place.map.kakao.com/${item.id}`, "_blank")
-              }
-            >
-              <span>{index + 1}.  </span>
-              <span>주소: {item.address_name}</span>
-              <span>도로명주소: {item.road_address_name}</span>
-              <span>가게명: {item.place_name}</span>
-              <span>전화번호: {item.phone}</span>
-              <div>코멘트: {item.comment}</div>
-            </div>
-            <div>
-              <button
-                onClick={() => {
-                  setData(item);
-                  listDelete(index);
-                }}
-              >
-                삭제
-              </button>
-              <Comment id={item.id} />
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <AllDiv>
+      <HeadH3>찜 목록</HeadH3>
+        <ResultListUl>
+          {list.map((item, index) => (
+            <ResultListLi>
+              <AllHeartListDiv>
+                  <ResultDiv>
+                    <div onClick={() => window.open(`http://place.map.kakao.com/${item.id}`, "_blank")} >
+                      <span><b>{index + 1}. </b></span>
+                      <span><strong>{item.place_name}</strong></span><br />
+                      <span>{item.road_address_name}</span><br />
+                      <span> ({item.address_name})</span><br />
+                      <span>☎ {item.phone}</span>
+                      </div>
+                      <Comment id={item.id} />
+                  </ResultDiv>
+                  <delBtnDiv>
+                        <delBtn onClick={() => {
+                                        setData(item);
+                                        listDelete(index);
+                                      }}
+                                    >삭제
+                        </delBtn>
+                  </delBtnDiv>
+              </AllHeartListDiv>
+            </ResultListLi>
+          ))}
+        </ResultListUl>
+    </AllDiv>
   );
 }
+
+const AllDiv = styled.div`
+  padding-left: 5px;
+`
+const ResultDiv = styled.div`
+  padding-left: 5px;
+`
+const AllHeartListDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  line-height: 22px;
+`;
+
+const ResultListUl = styled.ul`
+  list-style: none;
+  width: 600px;
+  margin: auto;
+`;
+
+const ResultListLi = styled.li`
+  cursor: pointer;
+  border-top: 1px solid gray;
+  border-bottom: 1px solid gray;
+  color: black;
+  padding: 10px;
+`;
+
+
+const HeadH3 = styled.h3`
+text-align: center;
+`;
+const delBtnDiv = styled.div`
+`;
+const delBtn = styled.button`
+`;
+
 
 export default KeepInfo;
